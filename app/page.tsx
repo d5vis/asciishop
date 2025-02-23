235365d61b9e441a7bb147c2da975d839cefd1a5
d5vis <davisjbanks@gmail.com>
2025-02-22 21:37:04 -0800

    modify editor placement

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Title from "@/components/title";
 export default function Home() {
   return (
     <div className="crt motion-blur-in-md motion-duration-400 grid grid-rows-[1fr_auto] items-center justify-center min-h-screen p-4 gap-2 font-[family-name:var(--font-vt323)]">
-      <div>
+      <div className="flex flex-col gap-4 justify-center w-full h-full">
         <header className="px-4">
           <Title />
         </header>
